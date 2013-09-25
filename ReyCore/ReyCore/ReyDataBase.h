d9c4acc755b01c22b63ee01b8409778b352d20e6
Rey <rey0@qq.com>
2013-09-25 15:53:55 +0800

    新增FMDB用来处理Sqlite.稍后会添加封装类.方便进行Sqlite操作

---
+++ ReyCore/ReyCore/ReyDataBase.h
@@ -0,0 +1,16 @@
+//
+//  ReyDataBase.h
+//  ReyCore
+//
+//  Created by Rey on 13-9-24.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface ReyDataBase : NSObject
+
+
+@end
---
+++ ReyCore/ReyCore/ReyDataBase.m
@@ -0,0 +1,16 @@
+//
+//  ReyDataBase.m
+//  ReyCore
+//
+//  Created by Rey on 13-9-24.
+//
+//
+
+#import "ReyDataBase.h"
+#import "FMDatabase.h"
+
+@implementation ReyDataBase
+
+
+
+@end
